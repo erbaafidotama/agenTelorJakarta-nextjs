19dfd6ce14e7bd76e5e053e64f8208c2b3ac726b
Erba Afidotama <erb4fidotama@gmail.com>
2021-07-06 17:03:57 +0700

    connect with supabase and dynamic contents

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,14 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect, FunctionComponent } from "react";
 import { HiOutlinePlusCircle, HiOutlineMinusCircle } from "react-icons/hi";
 import { IoMdCart } from "react-icons/io";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { v4 as uuid } from "uuid";
+import { supabase } from "../api";
 
 import TelorKampung from "../static/telor-kampung.jpg";
 import TelorNegeri from "../static/telor-negeri.jpg";
 
+const ListTelor = (props) => {
+  console.log("ListTelor", props);
+  return (
+    <div className="p-2 mb-5 border border-gray-300 rounded">
+      <div className="grid grid-flow-col grid-rows-3 gap-4">
+        <div className="row-span-3">
+          <div className="w-48 h-48">
+            <Image src={TelorKampung} alt="telor kampung" />
+          </div>
+        </div>
+        <div className="col-span-2">
+          <div className="flex justify-center mb-3">{props.jenis}</div>
+          <hr />
+        </div>
+        <div className="col-span-2 row-span-2">
+          <div className="mb-5">Harga: Rp {props.harga},-</div>
+          <div className="flex justify-between">
+            <div className="flex">
+              <HiOutlineMinusCircle
+                onClick={() => setCountTelorKampung(telorKampung - 1)}
+                className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
+              />
+              <input
+                className="w-20 text-lg text-center border-b border-gray-300"
+                type="text"
+                // value={telorKampung}
+                onChange={() => {
+                  console.log("1");
+                }}
+              />
+              {/* {telorKampung} kg */}
+              <HiOutlinePlusCircle
+                className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
+                onClick={() => setCountTelorKampung(telorKampung + 1)}
+              />
+            </div>
+            <div>
+              <button
+                // onClick={handleCheckout}
+                className=" py-2 px-4 rounded-xl text-white bg-green-tk hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ..."
+              >
+                <div className="flex justify-between space-x-2">
+                  <IoMdCart className="mt-1" />
+                  <div>Beli</div>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 function Content() {
   const [telorKampung, setCountTelorKampung] = useState("");
+  const [telors, setDataTelor] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const router = useRouter();
   const handleCheckout = (e) => {
@@ -16,105 +74,44 @@ function Content() {
     router.push("/checkout");
   };
 
+  useEffect(() => {
+    fetchTelor();
+  }, []);
+
+  async function fetchTelor() {
+    const { data, error } = await supabase.from("tbl_telor").select();
+    setDataTelor(data);
+    setLoading(false);
+  }
+
+  async function createNewPost() {
+    // if (!title || !content) return;
+    const user = supabase.auth.user();
+    const id = uuid();
+    // post.id = id;
+    const { data } = await supabase
+      .from("tbl_telor")
+      .insert([
+        {
+          jenis_telor: "Telor Kampung",
+          harga_telor: 15000,
+          telor_uuid: id,
+          description: "Telor Kampung",
+        },
+      ])
+      .single();
+  }
+
+  console.log("telors", telors);
+  if (loading) return <p className="text-2xl">Loading ...</p>;
   return (
     <div className="flex-grow py-5 md:px-96">
       <div className="p-5 bg-white shadow-xl ">
-        {/* Telor Kampung */}
-        <div className="p-2 mb-5 border border-gray-300 rounded">
-          <div className="grid grid-flow-col grid-rows-3 gap-4">
-            <div className="row-span-3">
-              <div className="w-48 h-48">
-                <Image src={TelorKampung} alt="telor kampung" />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <div className="flex justify-center mb-3">Telor Ayam Kampung</div>
-              <hr />
-            </div>
-            <div className="col-span-2 row-span-2">
-              <div className="mb-5">Harga: Rp 20.000,-</div>
-              <div className="flex justify-between">
-                <div className="flex">
-                  <HiOutlineMinusCircle
-                    onClick={() => setCountTelorKampung(telorKampung - 1)}
-                    className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
-                  />
-                  <input
-                    className="w-20 text-lg text-center border-b border-gray-300"
-                    type="text"
-                    value={telorKampung}
-                    onChange={() => {
-                      console.log("1");
-                    }}
-                  />
-                  {/* {telorKampung} kg */}
-                  <HiOutlinePlusCircle
-                    className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
-                    onClick={() => setCountTelorKampung(telorKampung + 1)}
-                  />
-                </div>
-                <div>
-                  <button
-                    onClick={handleCheckout}
-                    className=" py-2 px-4 rounded-xl text-white bg-green-tk hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ..."
-                  >
-                    <div className="flex justify-between space-x-2">
-                      <IoMdCart className="mt-1" />
-                      <div>Beli</div>
-                    </div>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Telor Negeri */}
-        <div className="p-2 mb-5 border border-gray-300 rounded ">
-          <div className="grid grid-flow-col grid-rows-3 gap-4">
-            <div className="row-span-3">
-              <div className="w-48 h-48">
-                <Image src={TelorNegeri} alt="telor negeri" />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <div className="flex justify-center mb-3">Telor Ayam Kampung</div>
-              <hr />
-            </div>
-            <div className="col-span-2 row-span-2">
-              <div className="mb-5">Harga: Rp 20.000,-</div>
-              <div className="flex justify-between">
-                <div className="flex">
-                  <HiOutlineMinusCircle
-                    onClick={() => setCountTelorKampung(telorKampung - 1)}
-                    className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
-                  />
-                  <input
-                    className="w-20 text-lg text-center border-b border-gray-300"
-                    type="text"
-                    value={telorKampung}
-                    onChange={() => {
-                      console.log("2");
-                    }}
-                  />
-                  {/* {telorKampung} kg */}
-                  <HiOutlinePlusCircle
-                    className="rounded-full cursor-pointer h-7 w-7 hover:bg-gray-400"
-                    onClick={() => setCountTelorKampung(telorKampung + 1)}
-                  />
-                </div>
-                <div>
-                  <button className=" py-2 px-4 rounded-xl text-white bg-green-tk hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
-                    <div className="flex justify-between space-x-2">
-                      <IoMdCart className="mt-1" />
-                      <div>Beli</div>
-                    </div>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        {
+          telors.map(telor => (
+            <ListTelor key={telor.id} jenis={telor.jenis_telor} harga={telor.harga_telor} />
+          ))
+        }
       </div>
     </div>
   );
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,32 @@
 import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
-import {IoLogoWhatsapp} from "react-icons/io"
+import { IoLogoWhatsapp } from "react-icons/io";
 import { IconContext } from "react-icons";
 
 function Footer() {
   return (
     <div className="p-2 border-t-2 border-gray-200">
-      <div className="flex justify-center mb-3 space-x-3">
-        <IconContext.Provider
-          value={{ color: "#03ac0e", className: "global-class-name" }}
-        >
-          <div>
-            <IoLogoWhatsapp className="w-7 h-7" />
-          </div>
-        </IconContext.Provider>
-        <div>088888888 (EA)</div>
+      <div className=" md:justify-around md:flex">
+        <div className="flex justify-center mb-3 space-x-3">
+          <IconContext.Provider
+            value={{ color: "#03ac0e", className: "global-class-name" }}
+          >
+            <div>
+              <IoLogoWhatsapp className="w-7 h-7" />
+            </div>
+          </IconContext.Provider>
+          <div>088888888 (EA)</div>
+        </div>
+        <div className="flex justify-center mb-3 space-x-3">
+          <FaPhoneAlt className="w-7 h-7" />
+          <div>088888888 (EA)</div>
+        </div>
+        <div className="flex justify-center mb-3 space-x-3">
+          Jalan Pancawarga 1, Jakarta Timur
+        </div>
       </div>
-      <div className="flex justify-center mb-3 space-x-3">
-        <FaPhoneAlt className="w-7 h-7" />
-        <div>088888888 (EA)</div>
-      </div>
-      <div className="flex justify-center mb-3 space-x-3">
-        Jalan Pancawarga 1, Jakarta Timur
+      <hr />
+      <div className="text-center">
+        Created by Erba
       </div>
     </div>
   );
